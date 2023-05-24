315169e436cde07a4ddb9e971b6157506aaff91a
Trayana Boykova <trayana.boykova@gmail.com>
2023-05-24 14:02:31 +0300

    Update P03_DecimalToBinary.java

--- src/Course01_StacksAndQueues/Lab/P03_DecimalToBinary.java
+++ src/Course01_StacksAndQueues/Lab/P03_DecimalToBinary.java
@@ -1,7 +1,36 @@
 package Course01_StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class P03_DecimalToBinary {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deque<Integer> bits = new ArrayDeque<>();
 
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num < 0) {
+            System.out.println("Cannot handle negative integers");
+            return;
+        }
+
+        if (num == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        while (num > 0) {
+            int bit = num % 2;
+            bits.push(bit);
+
+            num = num / 2;
+        }
+
+        while (!bits.isEmpty()) {
+            System.out.print(bits.pop());
+        }
     }
 }
+
