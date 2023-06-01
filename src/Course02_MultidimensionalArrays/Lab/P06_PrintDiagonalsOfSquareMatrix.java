362273276c1493f1cac7bae1f39df01071d1e5ab
Trayana Boykova <trayana.boykova@gmail.com>
2023-06-01 19:08:25 +0300

    Update P06_PrintDiagonalsOfSquareMatrix.java

--- src/Course02_MultidimensionalArrays/Lab/P06_PrintDiagonalsOfSquareMatrix.java
+++ src/Course02_MultidimensionalArrays/Lab/P06_PrintDiagonalsOfSquareMatrix.java
@@ -14,27 +14,14 @@ public class P06_PrintDiagonalsOfSquareMatrix {
         for (int row = 0; row < size; row++) {
             String[] columnData = scanner.nextLine().split(" ");
 
-//            int[] currentRow = new int[size];
-//            for (int col = 0; col < columnData.length; col++) {
-//                currentRow[col] = Integer.parseInt(columnData[col]);
-//            }
 
             int[] currentRow = Arrays.stream(columnData)
-                    .mapToInt(elem -> Integer.parseInt(elem))
+                    .mapToInt(Integer::parseInt)
                     .toArray();
 
             matrix[row] = currentRow;
         }
 
-//        Top left to bottom right
-//        for (int row = 0; row < matrix.length; row++) {
-//            for (int col = 0; col < matrix[row].length; col++) {
-//                if (row == col) {
-//                    System.out.print(matrix[row][col] + " ");
-//                }
-//            }
-//        }
-//        System.out.println();
 
         for (int i = 0; i < matrix.length; i++) {
             System.out.print(matrix[i][i] + " ");
@@ -51,4 +38,5 @@ public class P06_PrintDiagonalsOfSquareMatrix {
 
     }
 }
+
 
