3e18ca84b870f268feddc2db712fb551d724af60
Trayana Boykova <trayana.boykova@gmail.com>
2023-10-19 16:56:50 +0300

    updating tasks

---
+++ src/ExamsExercises/Exam_03/P03_Guild/Rank.java
@@ -0,0 +1,11 @@
+package ExamsExercises.Exam_03.P03_Guild;
+
+public class Rank {
+
+    public static final String MEMBER = "Member";
+    public static final String TRIAL = "Trial";
+
+    private Rank() {
+
+    }
+}
