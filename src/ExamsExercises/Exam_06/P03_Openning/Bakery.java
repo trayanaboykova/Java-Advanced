970078b692badb40fd56e38be05849e14101ac1b
Trayana Boykova <trayana.boykova@gmail.com>
2023-10-20 11:08:17 +0300

    updating tasks

---
+++ src/ExamsExercises/Exam_06/P03_Openning/Bakery.java
@@ -0,0 +1,55 @@
+package ExamsExercises.Exam_06.P03_Openning;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bakery {
+    private List<Employee> employees;
+    private String name;
+    private int capacity;
+
+    public Bakery(String name, int capacity) {
+        employees = new ArrayList<>();
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public void add(Employee employee) {
+        if (employees.size() < capacity) {
+            employees.add(employee);
+        }
+    }
+
+    public boolean remove(String name) {
+        return employees.removeIf(e -> e.getName().equals(name));
+    }
+
+    public Employee getOldestEmployee() {
+        return employees.stream()
+                .max(Comparator.comparingInt(Employee::getAge))
+                .orElse(null);
+
+    }
+
+    public Employee getEmployee(String name) {
+        return employees.stream().filter(e -> e.getName().equals(name)).findAny().orElse(null);
+    }
+
+    public int getCount() {
+        return employees.size();
+    }
+
+    public String report() {
+        StringBuilder output = new StringBuilder();
+        output.append(String.format("Employees working at Bakery %s:", this.name));
+
+        for (Employee employee : employees) {
+            output.append(System.lineSeparator());
+            output.append(employee.toString());
+        }
+        return output.toString();
+
+    }
+}
