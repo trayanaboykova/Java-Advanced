3f4930b950cc4f48252f99ab23df1e25e9b06bd8
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-15 12:11:07 +0300

    Update SmartArray.java

--- src/Lesson07_WorkshopCustomDataStructures/SmartArray.java
+++ src/Lesson07_WorkshopCustomDataStructures/SmartArray.java
@@ -1,5 +1,25 @@
 package Lesson07_WorkshopCustomDataStructures;
 
 public class SmartArray {
+    private int[] data;
+    private int index;
+    public SmartArray() {
+        this.data = new int[2];
+        this.index = 0;
+    }
+    public void add(int element) {
+        if (index == data.length) {
+           data = resize();
+        }
+        data[index] = element;
+        index++;
+    }
 
+    private int[] resize() {
+        int[] newData = new int[data.length + 1];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        return newData;
+    }
 }
