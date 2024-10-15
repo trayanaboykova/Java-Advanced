72b247295530d2e53e847a9337d2f08071855965
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-15 15:29:12 +0300

    Update SmartStack.java

--- src/Lesson07_WorkshopCustomDataStructures/SmartStack.java
+++ src/Lesson07_WorkshopCustomDataStructures/SmartStack.java
@@ -1,4 +1,37 @@
 package Lesson07_WorkshopCustomDataStructures;
 
 public class SmartStack {
+
+    // node structure
+    private Node top;
+    private int size;
+    private static class Node {
+        private int val;
+        private Node prev;
+
+        public Node(int val, Node prev) {
+            this.val = val;
+            this.prev = prev;
+        }
+    }
+
+    public void push(int element) {
+        top = new Node(element, top);
+        size++;
+    }
+
+    public int pop() {
+        int val = top.val;
+        top = top.prev;
+        size--;
+        return val;
+    }
+
+    public int peek() {
+        return top.val;
+    }
+
+    public int size() {
+        return size;
+    }
 }
