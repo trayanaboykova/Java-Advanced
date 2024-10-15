5d02104d7a33596060d180fc2b38e9e363e8026d
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-15 13:11:25 +0300

    Update Main.java

--- src/Lesson07_WorkshopCustomDataStructures/Main.java
+++ src/Lesson07_WorkshopCustomDataStructures/Main.java
@@ -8,25 +8,25 @@ public class Main {
 
         SmartArray smartArray = new SmartArray();
 
-        smartArray.add(13);
-        smartArray.add(3);
-        smartArray.add(65);
-        smartArray.add(34);
-
-        for (int i = 0; i < smartArray.size(); i++) {
-            System.out.println(smartArray.get(i));
-        }
+//        smartArray.add(13);
+//        smartArray.add(3);
+//        smartArray.add(65);
+//        smartArray.add(34);
+//
+//        for (int i = 0; i < smartArray.size(); i++) {
+//            System.out.println(smartArray.get(i));
+//        }
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        list.add(13);
-        list.add(2);
-        list.add(65);
-        list.add(34);
-
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+//        list.add(13);
+//        list.add(2);
+//        list.add(65);
+//        list.add(34);
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
 
     }
 }
