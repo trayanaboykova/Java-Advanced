b1c45f836097f9626f3bcd2902cb57fe5f830570
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-15 12:11:04 +0300

    Update Main.java

--- src/Lesson07_WorkshopCustomDataStructures/Main.java
+++ src/Lesson07_WorkshopCustomDataStructures/Main.java
@@ -1,7 +1,18 @@
 package Lesson07_WorkshopCustomDataStructures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        SmartArray smartArray = new SmartArray();
+
+        smartArray.add(13);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(13);
+
     }
 }
