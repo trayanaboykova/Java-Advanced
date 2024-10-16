f1268c0ec46d30799c894742bf0017e9e15bb666
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 10:49:07 +0300

    Update DoublyLinkedList.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
@@ -1,4 +1,32 @@
 package Lesson07_WorkshopCustomDataStructures.Exercise;
 
 public class DoublyLinkedList {
+    // глава - първият елемент(node) в списъка
+    private Node head;
+    // опашка - последният елемент(node) в списъка
+    private Node tail;
+    // размер
+    private int size;
+
+    public void addFirst(int value) {
+        // добавя елемент(node) в началото на списъка
+        // 1. създаваме нов node
+        Node newNode = new Node(value);
+        // 2. проверка дали списъка е празен
+        if (isEmpty()) {
+           // празен
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // не е празен
+            newNode.next = head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+    }
+    public boolean isEmpty() {
+        // true -> ако имаме празен списък
+        // false -> ако списъкът не е празен
+        return this.size == 0;
+    }
 }
