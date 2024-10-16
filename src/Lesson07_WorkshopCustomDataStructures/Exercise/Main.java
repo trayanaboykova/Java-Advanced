80a73a6f5acf316eaf2bcbc65b3657a327acf4d5
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 12:35:57 +0300

    Update Main.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
@@ -2,6 +2,9 @@ package Lesson07_WorkshopCustomDataStructures.Exercise;
 
 public class Main {
     public static void main(String[] args) {
-
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        linkedList.addFirst(1);
+        linkedList.addFirst(2);
+        linkedList.addFirst(3);
     }
 }
