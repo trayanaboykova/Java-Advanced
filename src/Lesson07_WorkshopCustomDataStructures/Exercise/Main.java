53011151fd4d83b6dbd5f92f6300d9214467d134
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 17:13:03 +0300

    Update Main.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
@@ -2,7 +2,9 @@ package Lesson07_WorkshopCustomDataStructures.Exercise;
 
 public class Main {
     public static void main(String[] args) {
+
         DoublyLinkedList linkedList = new DoublyLinkedList();
+
         linkedList.addFirst(1);
         linkedList.addFirst(2);
         linkedList.addFirst(3);
@@ -12,5 +14,8 @@ public class Main {
 
         linkedList.removeFirst();
         linkedList.removeLast();
+
+        linkedList.forEach(el -> System.out.println(el));
+
     }
 }
