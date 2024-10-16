a9f65963d76d29f85003658665ca684883549d82
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 15:44:38 +0300

    Update DoublyLinkedList.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
@@ -35,6 +35,41 @@ public class DoublyLinkedList {
             // не е празен
             // създаваме нов node
             Node newNode = new Node(value);
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+            this.size++;
+        }
+    }
+
+    public int get (int index) {
+        // стойността на съответния индекс в списъка
+        int result = 0; // елементът на дадения индекс
+        // 1. проверка дали индексът е валиден
+        checkValidIndex(index);
+        // 2. проверка дали индексът ми е в началото (от нулевия до средния индекс)
+        if (index <= this.size / 2) {
+            Node currentNode = this.head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            // елементът на дадения индекс
+            result = currentNode.currentValue;
+        } // индексът е в края (от средния до последния)
+        else {
+            Node currentNode = this.tail;
+            for (int i = size - 1; i > index ; i--) {
+                currentNode = currentNode.prev;
+            }
+            // елементът на дадения индекс
+            result = currentNode.currentValue;
+        }
+        return result;
+    }
+
+    private void checkValidIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("The index " + index + " is out of bounds!");
         }
     }
 
