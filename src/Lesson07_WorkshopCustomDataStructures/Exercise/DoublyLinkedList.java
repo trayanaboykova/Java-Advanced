0eb7938606f69ac365ce944c600dc19b3cc767fa
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 17:03:01 +0300

    Update DoublyLinkedList.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/DoublyLinkedList.java
@@ -67,6 +67,30 @@ public class DoublyLinkedList {
         return result;
     }
 
+    public int removeFirst() {
+        // премахва и връща първия елемент от списъка
+        // 1. проверка за празен списък
+        if (isEmpty()) {
+            throw new IllegalStateException("Error while removing element from empty list.");
+        }
+        // 2. списък с един елемент
+        else if (this.size == 1) {
+            // премахваме единствения наличен елемент
+            int removedElement = this.head.currentValue;
+            this.head = this.tail = null;
+            this.size--;
+            return removedElement;
+        }
+        // 3. списък с повече от един елемент
+        else {
+            int removedElement = this.head.currentValue; // премахнатия елемент
+            this.head = this.head.next;
+            this.head.prev = null;
+            this.size--;
+            return removedElement;
+        }
+    }
+
     private void checkValidIndex(int index) {
         if (index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException("The index " + index + " is out of bounds!");
