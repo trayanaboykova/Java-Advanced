e7228f1b88f8ccd8a1ef9f5a119269607fa587c5
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 15:44:40 +0300

    Update Main.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/Main.java
@@ -6,5 +6,8 @@ public class Main {
         linkedList.addFirst(1);
         linkedList.addFirst(2);
         linkedList.addFirst(3);
+        linkedList.addLast(4);
+
+        System.out.println(linkedList.get(2));
     }
 }
