6e09563945b0de6844db19cabbf6c1aaf765e223
Trayana Boykova <trayana.boykova@gmail.com>
2024-10-16 10:49:09 +0300

    Update Node.java

--- src/Lesson07_WorkshopCustomDataStructures/Exercise/Node.java
+++ src/Lesson07_WorkshopCustomDataStructures/Exercise/Node.java
@@ -2,4 +2,21 @@ package Lesson07_WorkshopCustomDataStructures.Exercise;
 
 // всеки елемент в нашия свързан списък
 public class Node {
+    // текуща целочислена стойност
+    public int currentValue;
+    // предишния елемент
+    public Node next;
+    // следващия елемент (node)
+    public Node prev;
+
+    // конструктор -> създава елемент по текуща стойност
+    public Node(int currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    // node (елемент) -> string
+    @Override
+    public String toString() {
+        return "" + this.currentValue;
+    }
 }
